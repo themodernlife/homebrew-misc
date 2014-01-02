c9b0b92e043864f563b876cc9f03543d8c9afe8a
Ian Hummel <ian@themodernlife.net>
2014-01-02 15:39:57 -0500

    Include revision sha1

--- elastic-mapreduce-ruby.rb
+++ elastic-mapreduce-ruby.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class ElasticMapreduceRuby < Formula
-  head 'https://github.com/tc/elastic-mapreduce-ruby'
+  head 'https://github.com/tc/elastic-mapreduce-ruby.git', :revision => 'a0842130086fcc618c83b86a40ca050d0bf66a98'
   
   def elastic_mapreduce_script
     <<-EOS.undent
