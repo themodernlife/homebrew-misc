e63ee6c7f8c61ad3839f43d53537f7a8f16288dc
Ian Hummel <ian@themodernlife.net>
2014-01-02 15:13:02 -0500

    Typo

--- elastic-mapreduce-ruby.rb
+++ elastic-mapreduce-ruby.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Elastic < Formula
+class ElasticMapreduceRuby < Formula
   homepage 'http://aws.amazon.com/developertools/2264'
   url 'http://elasticmapreduce.s3.amazonaws.com/elastic-mapreduce-ruby.zip'
   sha1 '200b48171890ebc374ad2f6caf51455504800a55'
