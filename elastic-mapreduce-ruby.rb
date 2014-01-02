9de5135f2a5687c35abe86ecddf0a65677962e5e
Ian Hummel <ian@themodernlife.net>
2014-01-02 15:37:29 -0500

    Use 1.9 compatibile version and set version to HEAD

--- elastic-mapreduce-ruby.rb
+++ elastic-mapreduce-ruby.rb
@@ -1,10 +1,8 @@
 require 'formula'
 
 class ElasticMapreduceRuby < Formula
-  homepage 'https://github.com/tc/elastic-mapreduce-ruby'
-  url 'https://github.com/tc/elastic-mapreduce-ruby/archive/master.tar.gz'
-  sha1 '69b2039ba72a9caf0a120f4d5b4e16bd6ecd54c1'
-
+  head 'https://github.com/tc/elastic-mapreduce-ruby'
+  
   def elastic_mapreduce_script
     <<-EOS.undent
       #!/bin/bash
