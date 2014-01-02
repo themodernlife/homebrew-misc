e9909252374414ba53fbb958273cb19a99876fe7
Ian Hummel <ian@themodernlife.net>
2014-01-02 15:21:53 -0500

    Add a script to launch EMR so LOAD_PATH is correct

--- elastic-mapreduce-ruby.rb
+++ elastic-mapreduce-ruby.rb
@@ -5,7 +5,15 @@ class ElasticMapreduceRuby < Formula
   url 'http://elasticmapreduce.s3.amazonaws.com/elastic-mapreduce-ruby.zip'
   sha1 '200b48171890ebc374ad2f6caf51455504800a55'
 
+  def elastic_mapreduce_script
+    <<-EOS.undent
+      #!/bin/bash
+      exec #{libexec}/elastic-mapreduce "$@"
+    EOS
+  end
+
   def install
     libexec.install Dir['*']
+    (bin/"elastic-mapreduce").write elastic_mapreduce_script
   end
 end
