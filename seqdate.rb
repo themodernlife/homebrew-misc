5a5f3c7a67048bd5bdf5f37a2b8c85fcb0a37b05
Ian Hummel <ian@themodernlife.net>
2014-01-07 17:08:05 -0500

    Fix recipe

--- seqdate.rb
+++ seqdate.rb
@@ -1,11 +1,19 @@
 require 'formula'
 
-class Seqdate < Formula
-  head 'https://github.com/themodernlife/seqdate.git'
+class SeqDate < Formula
+  head 'https://github.com/themodernlife/seqdate'
+  
+  def seqdate_script
+    <<-EOS.undent
+      #!/bin/bash
+      exec #{libexec}/bin/seqdate "$@"
+    EOS
+  end
 
   def install
     system "sbt", "universal:package-bin"
-    system 'unzip', Dir['target/universal/*.zip'], "-d", "target/universal/unzipped"
-    prefix.install Dir['target/universal/unzipped/*']
+    system 'unzip', Dir['target/universal/seqdate-0.1.zip'], "-d", "target/universal/unzipped"
+    libexec.install Dir['target/universal/unzipped/seqdate-0.1/*']
+    (bin/"seqdate").write seqdate_script
   end
 end
