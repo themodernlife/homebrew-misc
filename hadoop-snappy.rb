872778e0877553b30837a6c02ee4af84e0e1a7d5
Ian Hummel <ian@themodernlife.net>
2014-12-05 15:39:38 -0500

    Add sha1 and fix opt_prefix

--- hadoop-snappy.rb
+++ hadoop-snappy.rb
@@ -4,6 +4,7 @@ class HadoopSnappy < Formula
   homepage 'https://github.com/xerial/snappy-java'
 
   url 'https://github.com/xerial/snappy-java/archive/1.1.1.3.tar.gz'
+  sha1 '16670cc681edbea95c642b39787ab0caddded616'
 
   depends_on "sbt"
 
@@ -23,7 +24,7 @@ class HadoopSnappy < Formula
   def caveats; <<-EOS.undent
     Make sure you add the following lines to your .bashrc:
 
-    export HADOOP_CLASSPATH=#{libexec}/snappy-java-?????.jar:$HADOOP_CLASSPATH
+    export HADOOP_CLASSPATH=#{opt_prefix}/libexec/snappy-java-1.1.1.3.jar:$HADOOP_CLASSPATH
 
     EOS
   end
